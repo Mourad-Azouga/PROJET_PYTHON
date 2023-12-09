0785cfe40f268de3f3a11a6f612316dee95b029a
Mourad Azouga <mourad-azouga@outlook.fr>
2023-12-09 09:05:33 +0100

    I didnt commit yesterday and forgot what I added, should be in the changelog

--- GUI APP/header.py
+++ GUI APP/header.py
@@ -9,5 +9,4 @@ from kivy.uix.screenmanager import ScreenManager, Screen
 from kivy.uix.scrollview import ScrollView
 from kivy.uix.gridlayout import GridLayout
 from kivy.uix.screenmanager import SlideTransition
-from kivy.app import App  
-
+from kivy.app import App
